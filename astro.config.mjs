99eca13f8d7e437dda2d15f00b050a9d3798880a
Brendan Carpio <bfcarpio.git@mailgw.com>
2026-01-17 01:42:08 -0800

    properly set paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  site: "https://bfcarpio.github.io/rto-calculator",
+
+  site: "https://bfcarpio.github.io",
+  base: "/rto-calculator",
   // Configure client-side scripts
   vite: {
     build: {
